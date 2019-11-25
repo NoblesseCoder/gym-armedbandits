30c2951bfa4a62e0d71d99591391c1e8dc83961b
yagamiash <ashobot@gmail.com>
2019-11-24 23:54:04 -0500

    Edited init file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gym_armedbandits',
+      version='0.0.1',
+      url="https://github.com/NoblesseCoder/gym-armedbandits",
+      author="Ashik Poovanna",
+      description='k-armed bandits environment for OpenAI Gym',
+      license='MIT License',
+      install_requires=['gym-armedbandits'])
